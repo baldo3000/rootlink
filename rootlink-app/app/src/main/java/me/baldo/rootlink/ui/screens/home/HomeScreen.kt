6068f9350befe2d93d154e32ff33de90b6b5e341
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2025-05-26 17:02:34 +0200

    Home screen revamp

--- rootlink-app/app/src/main/java/me/baldo/rootlink/ui/screens/home/HomeScreen.kt
+++ rootlink-app/app/src/main/java/me/baldo/rootlink/ui/screens/home/HomeScreen.kt
@@ -1,6 +1,5 @@
 package me.baldo.rootlink.ui.screens.home
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,7 +9,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ArrowForward
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -19,7 +22,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -45,75 +47,101 @@ fun HomeScreen(
         Column(
             modifier = Modifier
                 .padding(innerPadding)
-                .fillMaxSize(),
+                .fillMaxSize()
+                .padding(horizontal = 16.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
                 text = stringResource(R.string.home_title),
                 style = MaterialTheme.typography.headlineLarge,
-                modifier = Modifier.padding(16.dp),
                 color = MaterialTheme.colorScheme.primary,
                 fontWeight = FontWeight.Bold,
                 textAlign = TextAlign.Center
             )
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
-                horizontalArrangement = Arrangement.SpaceEvenly
+            Spacer(Modifier.height(32.dp))
+            NavCard(
+                stringResource(R.string.screen_map),
+                description = stringResource(R.string.screen_map_description),
+                important = true,
+                icon = BottomBarTab.Map.icon
             ) {
-                QuickNavButton(
-                    stringResource(R.string.screen_map),
-                    BottomBarTab.Map.icon
-                ) {
-                    navController.navigate(RootlinkRoute.Map)
-                }
-                QuickNavButton(
-                    stringResource(R.string.screen_air_quality_map),
-                    BottomBarTab.AirQualityMap.icon
-                ) {
-                    navController.navigate(RootlinkRoute.AirQualityMap)
-                }
-                QuickNavButton(
-                    stringResource(
-                        R.string.screen_catalog
-                    ),
-                    BottomBarTab.Catalog.icon
-                ) {
-                    navController.navigate(RootlinkRoute.Catalog)
-                }
+                navController.navigate(RootlinkRoute.Map)
             }
-
-            Button(
-                onClick = { navController.navigate(RootlinkRoute.Map) },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
+            Spacer(Modifier.height(16.dp))
+            NavCard(
+                stringResource(R.string.screen_air_quality_map),
+                description = stringResource(R.string.screen_air_quality_map_description),
+                icon = BottomBarTab.AirQualityMap.icon
             ) {
-                Text(stringResource(R.string.home_explore_map))
+                navController.navigate(RootlinkRoute.AirQualityMap)
             }
-
-            Spacer(Modifier.height(128.dp))
+            Spacer(Modifier.height(16.dp))
+            NavCard(
+                stringResource(
+                    R.string.screen_catalog
+                ),
+                description = stringResource(R.string.screen_catalog_description),
+                icon = BottomBarTab.Catalog.icon
+            ) {
+                navController.navigate(RootlinkRoute.Catalog)
+            }
+            Spacer(Modifier.height(96.dp))
         }
     }
 }
 
 @Composable
-fun QuickNavButton(label: String, icon: ImageVector, onClick: () -> Unit) {
-    Column(
-        modifier = Modifier
-            .size(96.dp)
-            .clickable(
-                onClick = onClick,
-                role = Role.Button,
-                onClickLabel = label
-            )
-            .padding(8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+private fun NavCard(
+    label: String,
+    description: String,
+    icon: ImageVector,
+    important: Boolean = false,
+    onClick: () -> Unit
+) {
+    val containerColor = if (important) {
+        MaterialTheme.colorScheme.primary
+    } else {
+        MaterialTheme.colorScheme.secondary
+    }
+    val textColor = if (important) {
+        MaterialTheme.colorScheme.onPrimary
+    } else {
+        MaterialTheme.colorScheme.onSecondary
+    }
+
+    Card(
+        onClick = onClick,
+        colors = CardDefaults.cardColors(
+            containerColor = containerColor
+        )
     ) {
-        Icon(imageVector = icon, contentDescription = label, modifier = Modifier.size(48.dp))
-        Text(text = label, style = MaterialTheme.typography.labelSmall)
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Icon(imageVector = icon, contentDescription = label, modifier = Modifier.size(32.dp))
+            Spacer(Modifier.width(16.dp))
+            Column(
+                modifier = Modifier.weight(1f),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = label,
+                    style = MaterialTheme.typography.titleMedium,
+                    color = textColor,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = description,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = textColor
+                )
+            }
+            Icon(imageVector = Icons.AutoMirrored.Outlined.ArrowForward, contentDescription = null)
+        }
     }
 }
